2e7a91d3633394da0d854b2a9c0f591a98e5a973
bluesadi <liuyibo@stu.scu.edu.cn>
2020-01-30 22:02:15 +0800

    模板

---
+++ 模板.cpp
@@ -0,0 +1,16 @@
+#define LOCAL
+#define ms(x,v) memset(x,v,sizeof x)
+#define debug(x) cout << "debug:" << x << endl
+#define inf 1 << 30
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+#ifdef LOCAL
+    freopen("D:/ACM/Algorithm/test.in","r",stdin);
+#endif
+    
+}
